9fcefcd2681be76f22e42fd15a97d738b41be77b
sergei_vokhminov <rreaper@list.ru>
2025-01-11 02:17:01 +0300

    обновлен модуль проекта

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("materials/", include("materials.urls"), name="materials"),
+    path("users/", include("users.urls"), name="users"),
 ]
