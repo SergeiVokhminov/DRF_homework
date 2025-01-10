8b052ef39c798ae6e030271f5714784e781ae573
sergei_vokhminov <rreaper@list.ru>
2025-01-11 01:32:37 +0300

    обновлены настройки проекта

--- config/settings.py
+++ config/settings.py
@@ -20,10 +20,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     "rest_framework",
     "users",
-    "materials"
+    "materials",
 ]
 
 MIDDLEWARE = [
@@ -101,4 +100,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
-
