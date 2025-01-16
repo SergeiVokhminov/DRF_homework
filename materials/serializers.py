bcf55eb5a173986749c0d770f16e61a2cc9c224a
sergei_vokhminov <rreaper@list.ru>
2025-01-16 22:17:49 +0300

    обновлены сериализаторы приложений users, materials

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,15 +3,26 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    """Сериализатор для модели курса."""
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-
 class LessonSerializer(serializers.ModelSerializer):
     """Сериализатор для модели урока."""
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели курса."""
+    lessons_count = serializers.SerializerMethodField(read_only=True)
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = (
+            "title",
+            "description",
+            "lessons_count",
+            "lessons"
+        )
