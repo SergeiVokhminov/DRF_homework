57a7df0f76e9f8524dfa7e662098e8d00a454ebe
sergei_vokhminov <rreaper@list.ru>
2025-01-18 18:07:46 +0300

    обновлена модули приложения users

--- users/urls.py
+++ users/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from users.apps import UsersConfig
 from rest_framework.routers import DefaultRouter
 
-from users.views import UserViewSet
+from users.views import UserViewSet, PaymentListView
 
 app_name = UsersConfig.name
 
 router = DefaultRouter()
 router.register(r'user', UserViewSet, basename='user')
 
-urlpatterns = [] + router.urls
+urlpatterns = [
+    path("payments/", PaymentListView.as_view(), name="payments_list"),
+] + router.urls
--- users/views.py
+++ users/views.py
@@ -1,10 +1,21 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, generics
+from rest_framework.filters import OrderingFilter
 
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import User, Payment
+from users.serializers import UserSerializer, PaymentSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """Представления для пользовательской модели."""
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class PaymentListView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+
+    filter_backends = (DjangoFilterBackend, OrderingFilter,)
+    ordering_fields = ("date",)
+    filterset_fields = ("course", "lesson", "payment_method",)
