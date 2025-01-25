99bf6ec50c044fda9867dd27f0b10824f766ab43
sergei_vokhminov <rreaper@list.ru>
2025-01-25 12:16:00 +0300

    обновлены модули приложения users

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.permissions import AllowAny
-from rest_framework.routers import DefaultRouter
+# from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
