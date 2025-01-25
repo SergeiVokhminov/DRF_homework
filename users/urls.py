3d443ef94e6a5f36da2a6e7a16f980febc05dc1a
sergei_vokhminov <rreaper@list.ru>
2025-01-25 10:15:37 +0300

    обновлены модули приложения users

--- users/urls.py
+++ users/urls.py
@@ -1,19 +1,33 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from users.apps import UsersConfig
-from users.views import (PaymentListView, UserRetrieveApiView,
-                         UserUpdateApiView, UserViewSet)
+from users.views import (PaymentListView, UserCreateAPIView, UserDeleteView,
+                         UserDetailView, UserListView, UserUpdateView)
 
 app_name = UsersConfig.name
 
-router = DefaultRouter()
-router.register(r"user", UserViewSet, basename="user")
+# router = DefaultRouter()
+# router.register(r"user", UserViewSet, basename="user")
 
 urlpatterns = [
-    path("user/update/<int:pk>/", UserUpdateApiView.as_view(), name="user_update"),
     path(
-        "user/retrieve/<int:pk>/", UserRetrieveApiView.as_view(), name="user_retrieve"
+        "login/",
+        TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
+        name="login",
     ),
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(permission_classes=(AllowAny,)),
+        name="token_refresh",
+    ),
+    path("register/", UserCreateAPIView.as_view(), name="register"),
+    path("", UserListView.as_view(), name="user_list"),
+    path("user/<int:pk>/", UserDetailView.as_view(), name="user_detail"),
+    path("user/update/<int:pk>/", UserUpdateView.as_view(), name="user_update"),
+    path("user/delete/<int:pk>/", UserDeleteView.as_view(), name="user_delete"),
     path("payments/", PaymentListView.as_view(), name="payments_list"),
-] + router.urls
+]  # + router.urls
