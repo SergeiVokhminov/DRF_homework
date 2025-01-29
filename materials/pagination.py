6e1f741edb909855d9b22f4bcd893708e6d819c2
sergei_vokhminov <rreaper@list.ru>
2025-01-29 19:46:02 +0300

    добавлена пагинация и валидаторы в приложении materials

---
+++ materials/pagination.py
@@ -0,0 +1,8 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class PageSizePagination(PageNumberPagination):
+    """Разбивка данных на страницы."""
+    page_size = 15
+    page_size_query = "page_size"
+    max_page_size = 100
