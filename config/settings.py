8bda4dcf896b9bd1d15cec81869628275ff2696f
sergei_vokhminov <rreaper@list.ru>
2025-03-17 19:44:20 +0300

    корректировка docker файлов и настроек проекта

--- config/settings.py
+++ config/settings.py
@@ -62,11 +62,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-if 'test' in sys.argv:
+if "test" in sys.argv:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'test_db.sqlite3',
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "test_db.sqlite3",
         }
     }
 
